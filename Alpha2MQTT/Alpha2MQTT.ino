278285faa00fa36146d9c6498afd9173090bee36
David Carrel <carrel@honey.local>
2024-06-01 12:09:49 -0700

    Clean up left-over/unused code

--- Alpha2MQTT/Alpha2MQTT.ino
+++ Alpha2MQTT/Alpha2MQTT.ino
@@ -979,7 +979,6 @@ void mqttReconnect()
 			
 		}
 
-		if (!subscribed)
 #ifdef DEBUG
 		sprintf(_debugOutput, "MQTT Failed: RC is %d\r\nTrying again in five seconds...", _mqtt.state());
 		Serial.println(_debugOutput);
